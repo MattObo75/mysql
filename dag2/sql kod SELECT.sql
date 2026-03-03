d02be6092486439d68ba9d347fe0a2dfb10d112a
Mattias Eklund <mattias.eklund@ockelbook.se>
2026-03-03 23:23:21 +0100

    Upddaterar dag 2

--- dag2/sql kod SELECT.sql
+++ dag2/sql kod SELECT.sql
@@ -9,7 +9,7 @@ ORDER BY customers.country, orders.orderDate DESC
 /* ---- Visa restorder eller lagersaldo ok för alla produkter ---- */
 SELECT products.productName, products.pricePerpiece, products.stockQuantity, SUM(order_rows.numberOf) AS 'Sum ordered',
 products.stockQuantity - SUM(order_rows.numberOf) AS 'New stock quantity',
-IF(products.stockQuantity -  SUM(order_rows.numberOf) <= 0, 'Rest order','stock quantity OK') AS 'Check stock quantity'
+IF(products.stockQuantity -  SUM(order_rows.numberOf) <= 0, 'Backorder','stock quantity OK') AS 'Check stock quantity'
 FROM Products INNER JOIN order_rows ON products.productId = order_rows.productId
 GROUP BY products.productName
 
@@ -57,8 +57,56 @@ SELECT RAND();
 /* ---- slumptal mellan 1 och 10 ---- */
 SELECT FLOOR((RAND() * 10) + 1);
 
+/* ---- FUNKTIONER OCH BERÄKNINGAR ---- */
 
+DELIMITER | 
+CREATE FUNCTION sf_DateCheck(InputDate DATE)
+RETURNS VARCHAR(15)
+BEGIN
+	DECLARE OrderStatus VARCHAR(15);
+	
+	IF YEAR(InputDate) = 1998 THEN
+		SET OrderStatus = 'Aktuell';
+	ELSE
+		SET OrderStatus = 'Arkivera';
+	END IF;
 
+	RETURN OrderStatus;
+END |
 
+/* ---- Använd funktionen i en valig SELECT sats ---- */
+SELECT orders.orderId, orders.orderDate, orders.deliveryCountry,sf_DateCheck(orders.orderDate) AS 'Orderstatus'
+FROM orders
+ORDER BY orders.deliveryCountry
 
+/* ---- Procedur för att sätta momsvärde ---- */
+DELIMITER | 
+CREATE PROCEDURE sp_CalcVAT()
+BEGIN
+	DECLARE VATvalue REAL;
+	SET VATvalue = 0.25;
 
+	SELECT products.productName, ROUND(products.pricePerpiece * VATvalue,2) AS 'Moms'
+	FROM Products;
+END |
+
+/* ---- Kör proceduren ---- */
+CALL sp_CalcVAT
+
+/* ---- REDIGERINGSFRÅGOR ---- */
+
+/* ---- Skapa tabell för arkiverade ordrar ---- */
+CREATE TABLE order_archive(orderId INT(11), orderDate DATE DEFAULT NULL,
+CustomerId VARCHAR(5) DEFAULT NULL, employeeId INT(11), reqDeliverydate DATE DEFAULT NULL,
+deliveryPlace VARCHAR(25) DEFAULT NULL, deliveryCountry VARCHAR(25) DEFAULT NULL, 
+State VARCHAR(20) DEFAULT NULL, PRIMARY KEY(orderId)) 
+
+/* ---- Lägg in ordrar äldre än 1997 i order_archive ---- */
+INSERT INTO order_archive(orderId, orderDate, CustomerId, employeeId, reqDeliverydate, deliveryPlace, deliveryCountry)
+SELECT orders.orderId, orders.orderDate, orders.CustomerId, orders.employeeId, orders.reqDeliverydate ,orders.deliveryPlace, orders.deliveryCountry
+FROM orders
+WHERE orders.orderDate < '1997-01-01'
+
+/* ---- Ta bort ordrar med deliveryCountry USA och Brasilien ---- */
+DELETE FROM order_archive
+WHERE order_archive.deliveryCountry = 'USA' OR order_archive.deliveryCountry = 'Brasilien'
